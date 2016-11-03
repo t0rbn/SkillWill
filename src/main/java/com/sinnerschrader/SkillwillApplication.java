b0b5ae1805e9178795438bd27cdfa530a60d31a6
Torben Reetz <torben.reetz@sinnerschrader.com>
2016-11-03 16:48:57 +0100

    initial spring boot setup

---
+++ src/main/java/com/sinnerschrader/SkillwillApplication.java
@@ -0,0 +1,12 @@
+package com.sinnerschrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillwillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillwillApplication.class, args);
+	}
+}

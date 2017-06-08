fac38d924c1468271ee65b589e3104046d4532f8
Jani Komulainen <jani.komulainen@sinnerschrader.com>
2017-06-08 14:37:57 +0200

    header component styling

--- src/frontend/src/components/header/header.jsx
+++ src/frontend/src/components/header/header.jsx
@@ -5,24 +5,11 @@ import { Router, Link, browserHistory } from 'react-router'
 export default class Header extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			isNavOpen: false
-		}
-		this.handleClick = this.handleClick.bind(this)
 		this.checkUserIdCookie = this.checkUserIdCookie.bind(this)
-
 	}
 	componentDidMount() {
 		this.checkUserIdCookie()
 	}
-
-	handleClick(e) {
-		this.setState({
-			isNavOpen: !this.state.isNavOpen
-		})
-		this.checkUserIdCookie()
-	}
-
 	checkUserIdCookie() {
 		const user = Cookies.load("user")
 		if (user != this.state.userId) {
@@ -56,35 +43,14 @@ export default class Header extends React.Component {
 	}
 
 	render() {
-		return (
+		return(
 			<header className="mod-navigation-container">
-				<div className={`mod-navigation nav-open-is-${this.state.isNavOpen}`}>
+				<div className="mod-navigation">
 					<Link className="s2-logo" to="/"></Link>
-					<label className="burger" title="Show navigation" onClick={this.handleClick}>
-						<div className="burger-layer-wrapper">
-							<span className="burger-layer"></span>
-							<span className="burger-layer"></span>
-							<span className="burger-layer"></span>
-							<span className="burger-layer"></span>
-						</div>
-					</label>
 					<nav className="nav">
 						<ul className="nav-list">
 							<li className="nav-item">
-								<Link
-									onClick={this.handleClick}
-									className="nav-link"
-									to="/">
-									Suche
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link
-									onClick={this.handleClick}
-									className="nav-link"
-									to={`/my-profile/${this.returnMyProfileLink()}`}>
-									Dein Profil
-								</Link>
+								<Link className="nav-link" to={`/my-profile/${this.returnMyProfileLink()}`}>Dein Profil</Link>
 							</li>
 							{this.renderLogOut()}
 						</ul>

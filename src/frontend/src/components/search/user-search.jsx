4a49c17b618051e49d779943bcce9b64637559db
Felix G. Lehmann <felix-g.lehmann@sinnerschrader.com>
2017-06-12 18:21:35 +0200

    change dropdown method to setlocationfilter

--- src/frontend/src/components/search/user-search.jsx
+++ src/frontend/src/components/search/user-search.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SearchBar from './search-bar.jsx'
 import Dropdown from '../dropdown/dropdown.jsx'
 import SearchSuggestions from './search-suggestion/search-suggestions.jsx'
-import { getUserBySearchTerms, filterUserList } from '../../actions'
+import { getUserBySearchTerms, setLocationFilter } from '../../actions'
 import { connect } from 'react-redux'
 
 class UserSearch extends React.Component {
@@ -23,7 +23,7 @@ class UserSearch extends React.Component {
 	}
 
 	handleDropdownSelect(location) {
-			this.props.filterUserList(location)
+			this.props.setLocationFilter(location)
 	}
 
 	render() {
@@ -52,4 +52,4 @@ function mapStateToProps(state) {
 	}
 }
 
-export default connect(mapStateToProps, { getUserBySearchTerms, filterUserList })(UserSearch)
+export default connect(mapStateToProps, { getUserBySearchTerms, setLocationFilter })(UserSearch)

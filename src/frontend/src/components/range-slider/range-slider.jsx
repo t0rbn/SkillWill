e541bb678c05368dca2c08f90e3b888bac5d4480
Felix G. Lehmann <felix-g.lehmann@sinnerschrader.com>
2017-06-14 16:15:07 +0200

    control range slider with props

--- src/frontend/src/components/range-slider/range-slider.jsx
+++ src/frontend/src/components/range-slider/range-slider.jsx
@@ -2,38 +2,37 @@ import React from 'react'
 import { Router, Route, Link } from 'react-router'
 
 export default class RangeSlider extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            progress: "lvl" + this.props.defaultValue
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
+	constructor(props) {
+		super(props)
+		this.handleChange = this.handleChange.bind(this)
+	}
 
-    handleChange(e) {
-        e.preventDefault()
-        let val =  e.target.value
-        this.props.onSlide(val, this.props.type)
-    }
+	handleChange(e) {
+		const { type } = this.props
+		e.preventDefault()
+		let value = e.target.value
+		this.props.onSlide(value, type)
+	}
 
-    render() {
-        return(
-            <div className="range-slider">
-                <div className={`level-bar ${this.state.progress}`}></div>
-                <span className="step lvl1"></span>
-                <span className="step lvl2"></span>
-                <span className="step lvl3"></span>
-                <input type='range' min='0' max='3' value={this.props.defaultValue} defaultValue={this.props.defaultValue} onChange={this.handleChange} />
-                <div className="legend">
-                    {this.props.legend.map( (data, i) => {
-                        return (
-                            <div className={`legend-level-${i}`} key="{i}">
-															<span className="legend-label">{data}</span>
-														</div>
-                        )
-                    })}
-                </div>
-            </div>
-        )
-    }
+	render() {
+		const { value, legend } = this.props
+		return (
+			<div className="range-slider">
+				<div className={`level-bar lvl${value}`}></div>
+				<span className="step lvl1"></span>
+				<span className="step lvl2"></span>
+				<span className="step lvl3"></span>
+				<input type='range' min='0' max='3' value={`${value}`} onChange={this.handleChange} />
+				<div className="legend">
+					{legend.map((data, i) => {
+						return (
+							<div className={`legend-level-${i}`} key={data}>
+								<span className="legend-label">{data}</span>
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		)
+	}
 }

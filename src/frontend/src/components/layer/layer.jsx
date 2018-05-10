8f9a008c8f79155cfdea7d93ca35e536db27c32d
torree <torben.reetz@sinnerschrader.com>
2018-05-10 14:41:41 +0200

    Hotfix: fix my profile not opening due to errorneous next/prev button

--- src/frontend/src/components/layer/layer.jsx
+++ src/frontend/src/components/layer/layer.jsx
@@ -21,65 +21,69 @@ class Layer extends React.Component {
 	handleClose() {
 		// @todo: refactor for edge cases (e.g. admin interface)
 		browserHistory.push("/")
-
 		document.body.classList.remove('layer-open')
 
 		this.props.exitSkillsEditMode()
 	}
 
+	getUserIdByOffsetFromCurrent(users, offset) {
+		if (!users || users.length < 1) {
+			return null;
+		}
+
+		const userIds = users.map(user => user.id)
+		const currentIndex = userIds.indexOf(this.props.params.id)
+		const nextIndex = currentIndex + offset
+
+		// current user not found in list, that's an error
+		if (currentIndex < 0) {
+			return null
+		}
+
+		// next user for last user in list -> first user in list
+		if (nextIndex >= userIds.length) {
+			return userIds[0]
+		}
+
+		// prev user for first user in list -> last user in list
+		if (nextIndex < 0) {
+			return userIds[userIds.length -1]
+		}
+
+		const returnId = userIds[currentIndex + offset]
+		if (!returnId) {
+			return null
+		}
+		return returnId
+	}
+
 	getNextUserId(users) {
-		let arr = []
-
-		for (let x in users) {
-			arr.push(users[x].id)
-		}
-
-		let counter = arr.indexOf(this.props.params.id)
-
-		let nextProfile = arr[counter + 1]
-		if (nextProfile === undefined) {
-			nextProfile = arr[0]
-		}
-		return nextProfile
+		return this.getUserIdByOffsetFromCurrent(users, 1)
 	}
 
 	getPrevUserId(users) {
-		let arr = []
-
-		for (let x in users) {
-			arr.push(users[x].id)
-		}
-
-		let counter = arr.indexOf(this.props.params.id)
-
-		let prevProfile = arr[counter - 1]
-		if (prevProfile === undefined) {
-			prevProfile = arr[0]
-		}
-
-		return prevProfile
+		return this.getUserIdByOffsetFromCurrent(users, -1)
 	}
 
 	shouldArrowsBeShown(users) {
-		const inMyProfile = this.props.location.pathname.indexOf('my-profile')
-
-		if (inMyProfile < 0 && users !== undefined) {
-			return true
-		}
-		return false
+		const inMyProfile = window.location.pathname.indexOf('my-profile') >= 0
+		return !inMyProfile && (users && users.length > 1)
 	}
 
 	render() {
 		const { results: { users }} = this.props
+		const showArrows = this.shouldArrowsBeShown(users)
+		const prevUserId = showArrows ? this.getPrevUserId(users) : ''
+		const nextUserId = showArrows ? this.getNextUserId(users) : ''
 
 		return (
 			<div className="layer-container" name="containerid">
 				<Link onClick={this.handleClose} className="close-layer" />
 
 				<div className="btn-wrapper">
-					<div className={this.shouldArrowsBeShown(users) ? "layer-btns" : "no-btns"}>
-						<Link className="previous-arrow" to={`/profile/${this.getPrevUserId(users)}`} />
-						<Link className="next-arrow" to={`/profile/${this.getNextUserId(users)}`} />
+					<div className={showArrows ? "layer-btns" : "no-btns"}>
+						<Link className="previous-arrow" to={`/profile/${prevUserId}`} />
+						<Link className="next-arrow" to={`/profile/${nextUserId}`} />
 					</div>
 
 				<div className="layer">
